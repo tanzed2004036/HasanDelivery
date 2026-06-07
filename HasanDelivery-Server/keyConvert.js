abe413983ddec9c39ec0c3b39a062b84e96f97a9
Tanzed <tancuetcse22@gmail.com>
2026-06-07 10:54:40 +0600

    update

--- HasanDelivery-Server/keyConvert.js
+++ HasanDelivery-Server/keyConvert.js
@@ -2,3 +2,9 @@ const fs = require('fs');
 const key = fs.readFileSync('./hasvery--firebase-adminsdk.json', 'utf8')
 const base64 = Buffer.from(key).toString('base64')
 console.log(base64)
+
+git init
+git commit -m "first commit with"
+git branch -M main
+git remote add origin https://github.com/tanzed2004036/git-practice.git
+git push -u origin main
